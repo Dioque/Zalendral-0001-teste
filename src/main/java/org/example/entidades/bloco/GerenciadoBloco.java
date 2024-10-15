58f42dcd18069cf5617320a1535ed3db43fce5f8
Dioque <eduardodioque@gmail.com>
2024-10-14 21:22:03 -0300

    Atualização e otimização carregamento de mapa

--- src/main/java/org/example/entidades/bloco/GerenciadoBloco.java
+++ src/main/java/org/example/entidades/bloco/GerenciadoBloco.java
@@ -38,19 +38,31 @@ public class GerenciadoBloco {
     public void desenhaMapa(Graphics2D g2){
 
         JogadorJsonParaLeitura matrizJson = new JogadorJsonParaLeitura("target/classes/JsonConfigSprit/mapa/mapa1.json");
-        int matrizOtimizada = 40;
+        int matrizOtimizada = 20;
         int tamanhoBloco = 48;
 
         int cordenadaXCamera;
         int cordenadaYCamera;
 
 
-        int [][] matrizOtimizadaInterface = extraiCentralMatrix(matrizJson.pixelArt, matrizOtimizada);
+        int [][] matrizOtimizadaInterface = extraiCentralMatrix(matrizJson.pixelArt, matrizOtimizada, tamanhoBloco);
+
+        int limitetelaMinX = cameraX % tamanhoBloco;
+        int limitetelaminY = cameraY % tamanhoBloco;
 
         for (int y = 0; y < matrizOtimizadaInterface.length; y++) {
             for (int x = 0; x < matrizOtimizadaInterface[y].length; x++) {
-                cordenadaXCamera = x*tamanhoBloco-cameraX;
-                cordenadaYCamera = y*tamanhoBloco-cameraY;
+                if (limitetelaMinX >= tamanhoBloco) {
+                    limitetelaMinX = 0;
+                }
+                if (limitetelaminY >= tamanhoBloco) {
+                    limitetelaminY = 0;
+                }
+                cordenadaXCamera = x*tamanhoBloco+limitetelaMinX;
+                cordenadaYCamera = y*tamanhoBloco+limitetelaminY;
+
+                System.out.println(limitetelaMinX);
+
 
                 int cordIndex = matrizOtimizadaInterface[y][x];
                 g2.drawImage(bloco[cordIndex].imagem, cordenadaXCamera, cordenadaYCamera, null);
@@ -59,13 +71,13 @@ public class GerenciadoBloco {
         }
     }
 
-    public static int [][] extraiCentralMatrix(int [][] matrix, int matrizOtimizada){
+    public static int [][] extraiCentralMatrix(int [][] matrix, int matrizOtimizada, int bloco){
 
         int matrixY = matrix.length;
         int matrizX = matrix[0].length;
 
-        int centroMatrizY = matrixY / 2;
-        int centroMatrizX = matrizX / 2;
+        int centroMatrizY = (matrixY / 2) - cameraY/bloco;
+        int centroMatrizX = (matrizX / 2) - cameraX/bloco;
 
         int inicioMatrizOtimizadaY = centroMatrizY - matrizOtimizada / 2;
         int inicioMatrizOtimizadaX = centroMatrizX - matrizOtimizada / 2;
